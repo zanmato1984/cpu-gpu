499f14ef74c971aac8987300e2f58414f35932fa
zanmato1984 <zanmato1984@gmail.com>
2020-03-20 01:11:25 +0800

    Fix cpu timer

--- cpu.h
+++ cpu.h
@@ -5,7 +5,7 @@
 
 template <size_t n, typename T>
 void cpu(const T *input, T *output, size_t size) {
-  GpuTimer timer(std::string("CPU " + get_profile_name<n, T>(size)));
+  CpuTimer timer(std::string("CPU " + get_profile_name<n, T>(size)));
 
   for (size_t i = 0; i < size; i++)
     output[i] = instruction<n>(input[i]);
